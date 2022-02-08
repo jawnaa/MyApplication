3fcd61d7bef0c588fdc3fe64d4159d005deb9183
Jawnaa <taimmaabdelhalek2004@gmail.com>
2022-02-08 10:04:12 +0200

    product details

---
+++ app/src/main/java/com/example/myapplication/ProductDetailsActivity.java
@@ -0,0 +1,15 @@
+package  com.example.myapplication
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductDetailsActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_details);
+    }
+}
+
+
